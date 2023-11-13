c3fe025a8852550be6b8555a6e7da47b3194fcb7
Axel Pettersson <axel@pettersson.cc>
2023-11-13 23:02:17 +0100

    Finish task 2619

---
+++ typescript/src/2619.test.ts
@@ -0,0 +1,17 @@
+import "./2619";
+
+describe("2617. Array Prototype Last", () => {
+  it("example 1", () => {
+    const nums = [null, {}, 3];
+    const actual = nums.last();
+    const expected = 3;
+    expect(actual).toEqual(expected);
+  });
+
+  it("example 2", () => {
+    const nums: any[] = [];
+    const actual = nums.last();
+    const expected = -1;
+    expect(actual).toEqual(expected);
+  });
+});
