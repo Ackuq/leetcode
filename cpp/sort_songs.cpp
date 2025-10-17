53d7f1761ad56109e22dcfa86f997e0c71d7d316
Axel Pettersson <axel@pettersson.cc>
2025-10-17 21:01:28 +0200

    Refactor topSongs function and update main to test with multiple songs

--- cpp/sort_songs.cpp
+++ cpp/sort_songs.cpp
@@ -42,15 +42,39 @@ class TopSongs {
 
     return result;
   }
+
+  vector<Song> topSongs2(vector<Song>& user1, vector<Song>& user2, int n) {
+    int i1 = 0;
+    int i2 = 0;
+    vector<Song> result(n);
+    for (int i = 0; i < n; i++) {
+      auto s1 = user1[i1];
+      auto s2 = user2[i2];
+      if (s1.streams > s2.streams) {
+        result[i] = s1;
+        i1++;
+      } else if (s2.streams > s1.streams) {
+        result[i] = s2;
+        i2++;
+      } else if (s2.name.compare(s1.name) > 0) {
+        result[i] = s1;
+        i1++;
+      } else {
+        result[i] = s2;
+        i2++;
+      }
+    }
+    return result;
+  }
 };
 
 int main() {
   TopSongs solution;
 
-  vector<Song> user1 = {Song{"d", 2}};
-  vector<Song> user2 = {Song{"a", 2}};
+  vector<Song> user1 = {Song{"a", 2}, Song{"c", 1}};
+  vector<Song> user2 = {Song{"b", 2}, Song{"d", 1}};
 
-  auto topSongs = solution.topSongs(user1, user2, 2);
+  auto topSongs = solution.topSongs2(user1, user2, 3);
 
   for (auto s : topSongs) {
     cout << s.name << "-" << s.streams << endl;
