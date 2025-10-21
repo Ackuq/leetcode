228a3979a7840a1dde0e3034127aa6e769818b89
Axel Pettersson <axel@pettersson.cc>
2025-10-21 21:16:58 +0200

    Add solution for maximum path sum in a binary tree

---
+++ cpp/124.cpp
@@ -0,0 +1,28 @@
+using namespace std;
+
+#include <algorithm>
+#include <climits>
+
+#include "./treenode.cpp"
+
+class Solution {
+ public:
+  int maxPathSum(TreeNode* root) {
+    maxSum = INT_MIN;
+    maxPathRec(root);
+    return maxSum;
+  }
+
+ private:
+  int maxSum;
+  int maxPathRec(TreeNode* curr) {
+    if (curr == nullptr) return 0;
+
+    int maxFromLeft = max(maxPathRec(curr->left), 0);
+    int maxFromRight = max(maxPathRec(curr->right), 0);
+
+    maxSum = max(maxSum, curr->val + maxFromLeft + maxFromRight);
+
+    return curr->val + max(maxFromLeft, maxFromRight);
+  }
+};
